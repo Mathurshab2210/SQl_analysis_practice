fb3359c2971423f754f1d889d750d5f0c380a750
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2023-04-28 11:26:05 +0530

    Update zomato_analysis.sql

--- zomato_analysis.sql
+++ zomato_analysis.sql
@@ -73,3 +73,59 @@ cte2 as (select * ,
      )
      select * from cte2 where rnk=1;
      
+-- what is total order and money spent just before the become gold members
+-- i have used view here
+select j.userid,count(j.product_id),
+sum(p.price) as amt
+from just_befor_gold j 
+join product p
+on p.product_id=j.product_id
+group by j.userid;
+
+-- If buying each product generates points for eg 5rs-2 zomato point and each product has different purchasing points
+-- for eg for pl 5rs-1 zomato point, for p2 10rs=5zomato point and p3 5rs-1 zomato point
+with cte1 as (select s.userid,s.product_id,p.price
+	from sales s 
+    join product p
+    on s.product_id=p.product_id
+    ), cte2 as(
+    select userid,product_id,sum(price) as tot_amt from cte1 
+		group by userid,product_id
+        order by userid),cte3 as(
+	 select *,
+     case 
+     when product_id=1 then 5
+	 when product_id=2 then 2
+	 when product_id=3 then 5
+     else 0
+     end as pt
+     from cte2
+      ) , cte4 as(select *,(tot_amt/pt) as zomato_points
+		from cte3)
+        select userid,sum(zomato_points) as tot_zp,
+			sum(zomato_points)*2.5 as cashback
+        from cte4
+        group by userid;
+        
+with cte1 as (select s.userid,s.product_id,p.price
+	from sales s 
+    join product p
+    on s.product_id=p.product_id
+    ), cte2 as(
+    select userid,product_id,sum(price) as tot_amt from cte1 
+		group by userid,product_id
+        order by userid),cte3 as(
+	 select *,
+     case 
+     when product_id=1 then 5
+	 when product_id=2 then 2
+	 when product_id=3 then 5
+     else 0
+     end as pt
+     from cte2
+      ) , cte4 as(select *,(tot_amt/pt) as zomato_points
+		from cte3)
+        select product_id,sum(zomato_points) as tot_zp_pr
+        from cte4
+        group by product_id
+     
