256006ef360ff97322af447f5adcfecd249bfd5e
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2024-12-21 23:34:34 +0530

    Create practice_problems.sql

---
+++ Retail_analysis/practice_problems.sql
@@ -0,0 +1,7 @@
+-- Practice Problems 
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of Nov-2022
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+-- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
+-- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
